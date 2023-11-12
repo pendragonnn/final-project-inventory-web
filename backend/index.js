251750ea08cfb1ee35260a04b77c876335b1dbaf
fahrul <fahruluyah12@gmail.com>
2023-11-12 15:38:14 +0700

    menambahkan crud outlet

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");;
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv")
+
+const app = express();
+dotenv.config()
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const router = require("./src/routes/index")
+app.use(router)
+
+const PORT = process.env.AppPORT
+
+app.listen(PORT, () => {
+  console.log(`Server running on https://localhost: ${PORT}`);
+});
