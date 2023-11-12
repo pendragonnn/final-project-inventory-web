9eb6f9a2a97f262505f46b2c86b011f24a41f6ed
fahrul <fahruluyah12@gmail.com>
2023-11-12 15:39:53 +0700

    menambahkan crud outlet

---
+++ backend/src/controller/userCont.js
@@ -0,0 +1,99 @@
+const usersevice = require("../services/userservice");
+const fs = require("fs");
+
+const getAllUser = async (req, res) => {
+  try {
+    const users = await usersevice.getAllUser();
+    res.status(200).json({ data: users });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await usersevice.getUserById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ data: user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createNewUser = async (req, res) => {
+  try {
+    const userData = req.body;
+    await usersevice.createNewUser(userData);
+    res.status(200).json({ message: "New user created" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userData = req.body;
+    const updateduser = await usersevice.updateUserById(id, userData);
+
+    if (!updateduser) {
+      return res.status(404).json({ message: " User not found " });
+    }
+    res.status(200).json({ message: " user update succesfully " });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deleteduser = await usersevice.deleteUserById(id);
+    if (!deleteduser) {
+      return res.status(404).json({ message: " User not found" });
+    }
+    res.status(200).json({ message: " User deleted succesfully " });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const uploadUserPhoto = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await usersevice.getUserById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
+
+    const image_url = req.file.path;
+
+    fs.unlinkSync(req.file.path);
+
+    const updatedUser = await usersevice.updateUserPhoto(id, image_url);
+
+    res
+      .status(200)
+      .json({ message: "User photo updated successfully", data: updatedUser });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getAllUser,
+  getUserById,
+  createNewUser,
+  updateUserById,
+  deleteUserById,
+  uploadUserPhoto,
+};
---
+++ backend/src/repository/userRepo.js
@@ -0,0 +1,58 @@
+const models = require("../../models");
+const User = models.User;
+
+const getAllUser = async (req, res) => {
+  return await User.findAll();
+};
+
+const getUserById = async (id) => {
+  return await User.findByPk(id);
+};
+
+const createNewUser = async (userData) => {
+  return await User.create(userData);
+};
+
+const updateUserById = async (id, userData) => {
+  const user = await User.findByPk(id);
+
+  if (!user) {
+    return null;
+  }
+  await User.update(userData, { where: { id } });
+  return user;
+};
+
+const deleteUserById = async (id) => {
+  const user = await User.findByPk(id);
+
+  if (!user) {
+    return null;
+  }
+  await User.destroy({ where: { id } });
+  return user;
+};
+
+const updateUserPhoto = async (id, image_url) => {
+  try {
+    const updatedUser = await User.update(
+      { image_url: image_url },
+      { where: { id }, returning: true }
+    );
+
+    if (updatedUser[0] === 0) return null;
+
+    return updatedUser[1][0].dataValues;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  getAllUser,
+  getUserById,
+  createNewUser,
+  updateUserById,
+  deleteUserById,
+  updateUserPhoto,
+};
