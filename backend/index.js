1563d5d0e7035e2aee84bf638e500dc22c2070a1
dittrmdhn <adityaramadhan63.ar@gmail.com>
2023-11-14 00:36:42 +0700

    fix

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,18 @@
-
-const express = require("express");;
+const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const itemRouter = require("./src/route/items")
+const itemRouter = require("./src/route/items");
 const app = express();
 
-dotenv.config()
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+dotenv.config();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const router = require("./src/route/index")
-app.use(router)
-app.use(itemRouter)
+const router = require("./src/route/index");
+app.use(router);
 
-const PORT = process.env.AppPORT
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-
-  console.log(`Server running on https://localhost: ${PORT}`)
-})
-
+  console.log(`Server running on https://localhost: ${PORT}`);
+});
