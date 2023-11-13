5149e7e3ab6cf6bc6dc8f07e446dfe5ddb224818
ap-pamungkas <agustinusputra94@gmail.com>
2023-11-14 00:30:53 +0700

    fixed models

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 const fs = require("fs");
@@ -11,23 +10,6 @@ const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 let sequelize;
-=======
-
-
-"use strict"
-
-const fs = require("fs")
-const path = require("path")
-const Sequelize = require("sequelize")
-const process = require("process")
-const basename = path.basename(__filename)
-const env = process.env.NODE_ENV || "development"
-const config = require(__dirname + "/../config/config.js")[env]
-const db = {}
-
-let sequelize
-
->>>>>>> c29588e46941271a99bd2703f39c74fb02b7b553
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config)
 } else {
