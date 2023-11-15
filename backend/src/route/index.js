dcb1708a7ca3c49e401939ea9195fd88d707582a
Gilang Fatahilah Akbar <gilangf010@gmail.com>
2023-11-15 20:24:10 +0700

    fix route

--- backend/src/route/index.js
+++ backend/src/route/index.js
@@ -1,28 +1,19 @@
-const express = require("express")
-const router = express.Router()
-const transactionheaderRoutes = require("./transactionheader.route")
-const transactiondetailRoutes = require("./transactiondetail.route")
-const suppliersRoutes = require("../route/supplier.route")
-const outletsRoutes = require("../route/outlet.route")
-const userRouter = require("./userRouter")
-const itemRouter = require("./items")
-
-<<<<<<< HEAD
+const express = require("express");
+const router = express.Router();
+const transactionheaderRoutes = require("./transactionheader.route");
+const transactiondetailRoutes = require("./transactiondetail.route");
 const suppliersRoutes = require("../route/supplier.route");
-router.use("/", suppliersRoutes);
 const outletsRoutes = require("../route/outlet.route");
-router.use("/", outletsRoutes);
 const userRouter = require("./userRouter");
-router.use("/", userRouter);
 const itemRouter = require("./items");
-router.use("/", itemRouter)
-=======
-router.use("/transaction_header", transactionheaderRoutes)
-router.use("/transaction_detail", transactiondetailRoutes)
-router.use("/", itemRouter)
-router.use("/", suppliersRoutes)
-router.use("/", outletsRoutes)
-router.use("/", userRouter)
->>>>>>> c29588e46941271a99bd2703f39c74fb02b7b553
+const authRoutes = require("./auth.route");
 
-module.exports = router
+router.use("/", authRoutes);
+router.use("/transaction_header", transactionheaderRoutes);
+router.use("/transaction_detail", transactiondetailRoutes);
+router.use("/", itemRouter);
+router.use("/", suppliersRoutes);
+router.use("/", outletsRoutes);
+router.use("/", userRouter);
+
+module.exports = router;
