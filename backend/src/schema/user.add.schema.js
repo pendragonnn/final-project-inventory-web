cbd17eea4309d9e79d8d98fe81ac3a8fec580e42
fahrul <fahruluyah12@gmail.com>
2023-11-23 12:06:47 +0700

    delete joi username

--- backend/src/schema/user.add.schema.js
+++ backend/src/schema/user.add.schema.js
@@ -4,7 +4,6 @@ const userAddSchema = Joi.object({
   role_id: Joi.number().required(),
   full_name: Joi.string().required(),
   email: Joi.string().email(),
-  username: Joi.string().required(),
   password: Joi.string().required(),
   image_url: Joi.string()
 })
