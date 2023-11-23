2c45f613263a5d7e9b48b25ed28df0ba567b216b
fahrul <fahruluyah12@gmail.com>
2023-11-23 15:19:10 +0700

    User Update

--- frontend/src/app/tablesUsers/page.jsx
+++ frontend/src/app/tablesUsers/page.jsx
@@ -1,5 +1,5 @@
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-
+import SidebarLayout from "../sidebar-layout";
 import TableUser from "@/components/Tables/TableUser";
 export const metadata = {
   title: "Tables Page | Next.js E-commerce Dashboard Template",
@@ -8,13 +8,13 @@ export const metadata = {
 };
 const TablesPage = () => {
   return (
-    <>
+    <SidebarLayout>
       <Breadcrumb pageName="Tables" />
 
       <div className="flex flex-col gap-10">
         <TableUser />
       </div>
-    </>
+    </SidebarLayout>
   );
 };
 export default TablesPage;
