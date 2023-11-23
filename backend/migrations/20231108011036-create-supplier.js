00ca54a9be61e39193668a3e8b0b1135c95e4e26
dittrmdhn <adityaramadhan63.ar@gmail.com>
2023-11-23 09:42:27 +0700

    fix

--- backend/migrations/20231108011036-create-supplier.js
+++ backend/migrations/20231108011036-create-supplier.js
@@ -1,33 +1,33 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Suppliers', {
+    await queryInterface.createTable("Suppliers", {
       id: {
         allowNull: false,
         primaryKey: true,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
-      supplier: {
-        type: Sequelize.STRING
+      name: {
+        type: Sequelize.STRING,
       },
       address: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       phone: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Suppliers');
-  }
+    await queryInterface.dropTable("Suppliers");
+  },
 };
--- backend/models/supplier.js
+++ backend/models/supplier.js
@@ -1,22 +1,23 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Supplier extends Model {
     static associate(models) {
       Supplier.hasMany(models.TransactionHeader, {
-        foreignKey: 'supplier_id'
-      })
+        foreignKey: "supplier_id",
+      });
     }
   }
-  Supplier.init({
-    supplier: DataTypes.STRING,
-    address: DataTypes.STRING,
-    phone: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Supplier',
-  });
+  Supplier.init(
+    {
+      name: DataTypes.STRING,
+      address: DataTypes.STRING,
+      phone: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Supplier",
+    }
+  );
   return Supplier;
 };
