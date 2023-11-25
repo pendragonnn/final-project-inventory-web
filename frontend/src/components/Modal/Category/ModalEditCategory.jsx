404eb2fc9fefc1986a38549df254e4388616f512
Wisnu Andika <wisnuandika104@gmail.com>
2023-11-25 13:34:58 +0700

    feat: add category page

---
+++ frontend/src/components/Modal/Category/ModalEditCategory.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import Swal from "sweetalert2";
+import axios from "axios";
+import Category from "@/data/category/index";
+import { useRef } from "react";
+
+const ModalEditCategory = ({ data, test, addToTable }) => {
+  const modalCheckbox = useRef(null);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await Category.updateCategory(data.id, {
+        name: e.target.name.value,
+      });
+
+      Swal.fire({
+        position: "bottom-end",
+        icon: "success",
+        title: res.data.message,
+        showConfirmButton: false,
+        timer: 2000,
+        customClass: "swal-custom",
+      }).then(() => {
+        addToTable(res.data.data);
+        modalCheckbox.current.checked = false;
+      });
+    } catch (e) {
+      Swal.fire({
+        position: "bottom-end",
+        icon: "error",
+        title: e.message,
+        showConfirmButton: false,
+        timer: 2000,
+        customClass: "swal-custom",
+      });
+    }
+  };
+
+  return (
+    <>
+      <input
+        type="checkbox"
+        ref={modalCheckbox}
+        id={test}
+        className="modal-toggle"
+      />
+      <div className="modal" role="dialog">
+        <div className="modal-box bg-white dark:bg-boxdark">
+          <label
+            htmlFor={test}
+            className="bg-[#F1F3FB] dark:bg-graydark dark:text-white w-9 h-9 rounded-full flex items-center justify-center float-right cursor-pointer text-xl text-[#6A718A] hover:text-primary"
+          >
+            x
+          </label>
+          <div className="rounded-sm bg-white dark:bg-boxdark">
+            <div className=" py-4 px-6.5 ">
+              <h3 className="font-medium text-black dark:text-white">
+                Edit data Category
+              </h3>
+            </div>
+
+            <form action="#" onSubmit={handleSubmit}>
+              <div className="p-6.5 text-start">
+                <div className="mb-4.5">
+                  <label className="mb-2.5 block text-black dark:text-white">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    defaultValue={data?.name}
+                    placeholder="Enter full name"
+                    className="w-full rounded border-[1.5px] text-black dark:text-white border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                    required
+                  />
+                </div>
+
+                <input
+                  type="submit"
+                  value={"edit"}
+                  className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ModalEditCategory;
