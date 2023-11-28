63f4b4fd5f3f186e96f26135f45254b98df3feb8
fahrul <fahruluyah12@gmail.com>
2023-11-28 12:54:37 +0700

    y

--- frontend/src/components/Modal/User/ModalAddUser.jsx
+++ frontend/src/components/Modal/User/ModalAddUser.jsx
@@ -1,4 +1,181 @@
-import React from "react";
+// import React, { useRef } from "react";
+// import Swal from "sweetalert2";
+// import axios from "axios";
+
+// const ModalUserAdd = ({ name, test, addToTable }) => {
+//   const modalCheckbox = useRef(null);
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const haha = {
+//         role_id: e.target.role_id.value,
+//         full_name: e.target.full_name.value,
+//         email: e.target.email.value,
+//         password: e.target.password.value,
+//       }
+
+      
+
+//       const res = await axios.post("http://localhost:8000/user", haha);
+//       const userId = res.data.data.id;
+
+//       const formData = {
+//         image_url: e.target.image_url.value
+//       }
+
+//       const imageResponse = await axios.post(`http://localhost:8000/user/upload/${userId}`, formData, {
+//         headers: {
+//           "Content-Type": "multipart/form-data",
+//         },
+//       });
+
+//       console.log(imageResponse);
+//       console.log(userId);
+//       console.log(res);
+//       Swal.fire({
+//         position: "bottom-end",
+//         icon: "success",
+//         title: res.data.message,
+//         showConfirmButton: false,
+//         timer: 2000,
+//         customClass: "swal-custom",
+//       }).then(() => {
+//         addToTable(res.data.data);
+//         modalCheckbox.current.checked = false;
+//       });
+//     } catch (e) {
+//       Swal.fire({
+//         position: "bottom-end",
+//         icon: "error",
+//         title: e.message,
+//         showConfirmButton: false,
+//         timer: 2000,
+//         customClass: "swal-custom",
+//       });
+//     }
+//   };
+
+//   return (
+//     <>
+//       <label htmlFor={test} className="cursor-pointer">
+//         {name}
+//       </label>
+//       <input
+//         type="checkbox"
+//         id={test}
+//         ref={modalCheckbox}
+//         className="modal-toggle"
+//       />
+//       <div className="modal" role="dialog">
+//         <div className="modal-box bg-white dark:bg-boxdark">
+//           <label
+//             htmlFor={test}
+//             className="bg-[#F1F3FB] dark:bg-graydark dark:text-white w-9 h-9 rounded-full flex items-center justify-center float-right  text-xl text-[#6A718A] hover:text-primary"
+//           >
+//             x
+//           </label>
+//           <div className="rounded-sm bg-white dark:bg-boxdark">
+//             <div className=" py-4 px-6.5 ">
+//               <h3 className="font-medium text-black dark:text-white">
+//                 Add new User
+//               </h3>
+//             </div>
+
+//             <form action="#" onSubmit={handleSubmit}>
+//               <div className="p-6.5 text-start">
+//                 <div className="mb-4.5">
+//                   <label className="block text-black dark:text-white">
+//                     Role
+//                   </label>
+
+//                   <label
+//                     for="countries"
+//                     class="block  text-sm font-medium text-gray-900 dark:text-white"
+//                   >
+//                     Select an option
+//                   </label>
+//                   <select
+//                     className="w-full rounded border-[1.5px] text-black dark:text-white border-stroke bg-transparent py-3 px-4 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+//                     name="role_id"
+//                   >
+//                     <option selected>Choose a role</option>
+//                     <option value="1">Admin</option>
+//                     <option value="2">Staff</option>
+//                     <option value="3">Manager</option>
+//                   </select>
+//                 </div>
+
+//                 <div className="mb-4.5">
+//                   <label className="mb-2.5 block text-black dark:text-white">
+//                     Name
+//                   </label>
+//                   <input
+//                     type="text"
+//                     name="full_name"
+//                     placeholder="Enter full name"
+//                     className="w-full rounded border-[1.5px] text-black dark:text-white border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+//                     required
+//                   />
+//                 </div>
+
+//                 <div className="mb-4.5">
+//                   <label className="mb-2.5 block text-black dark:text-white">
+//                     Email
+//                   </label>
+//                   <input
+//                     type="text"
+//                     name="email"
+//                     placeholder="Enter Email"
+//                     className="w-full rounded border-[1.5px] text-black dark:text-white border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+//                     required
+//                   />
+//                 </div>
+
+//                 <div className="mb-4.5">
+//                   <label className="mb-2.5 block text-black dark:text-white">
+//                     Password
+//                   </label>
+//                   <input
+//                     type="text"
+//                     name="password"
+//                     placeholder="Enter password"
+//                     className="w-full rounded border-[1.5px] text-black dark:text-white border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+//                     required
+//                   />
+//                 </div>
+
+//                 <div className="mb-4.5">
+//           <label className="mb-2.5 block text-black dark:text-white">
+//             Profile Photo
+//           </label>
+//           <input
+//             type="file"
+//             name="image_url"
+//             key={"user-photo"}
+//             accept="image/*"
+//             className="w-full text-black dark:text-white border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+            
+//           />
+//         </div>
+
+//                 <input
+//                   type="submit"
+//                   value={"Add"}
+//                   className="flex w-full justify-center rounded cursor-pointer bg-primary p-3 font-medium text-gray"
+//                 />
+//               </div>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default ModalUserAdd;
+
+
+import React, { useRef, useState } from "react";
 import Swal from "sweetalert2";
 import axios from "axios";
 import UserData from "@/data/user/index";
@@ -113,12 +290,7 @@ const ModalUserAdd = ({ name, test, addToTable }) => {
                   <label className="block text-black dark:text-white">
                     Role
                   </label>
-                <div className="mb-4.5">
-                  <label className="block text-black dark:text-white">
-                    Role
-                  </label>
 
-                  <label
                   <label
                     htmlFor="countries"
                     className="block  text-sm font-medium text-gray-900 dark:text-white"
@@ -136,10 +308,6 @@ const ModalUserAdd = ({ name, test, addToTable }) => {
                 </div>
 
                 <div className="mb-4.5">
-                  <label className="mb-2.5 block text-black dark:text-white">
-                    Name
-                  </label>
-                  <input
                   <label className="mb-2.5 block text-black dark:text-white">
                     Name
                   </label>
