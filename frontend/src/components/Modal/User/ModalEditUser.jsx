86f9aa247f56b70a7985a2161453f6568d95c3d8
fahrul <fahruluyah12@gmail.com>
2023-11-30 11:45:49 +0700

    update user

--- frontend/src/components/Modal/User/ModalEditUser.jsx
+++ frontend/src/components/Modal/User/ModalEditUser.jsx
@@ -5,26 +5,9 @@ import UserData from "@/data/user/index";
 const ModalEditUser = ({ data, test, addToTable }) => {
   const modalCheckbox = useRef(null);
   const [file, setFile] = useState(null);
-  const [imagePreview, setImagePreview] = useState(null);
 
   const handleFileChange = (e) => {
-    const selectedFile = e.target.files[0];
-
-    if (selectedFile) {
-      setFile(selectedFile);
-
-      // Create a FileReader instance
-      const reader = new FileReader();
-
-      // Set a callback function to be triggered when the file is loaded
-      reader.onloadend = () => {
-        // Update the image preview state with the data URL of the loaded file
-        setImagePreview(reader.result);
-      };
-
-      // Read the selected file as a data URL
-      reader.readAsDataURL(selectedFile);
-    }
+    setFile(e.target.files[0]);
   };
 
   const handleSubmit = async (e) => {
@@ -39,7 +22,7 @@ const ModalEditUser = ({ data, test, addToTable }) => {
         email: e.target.email.value,
       };
 
-      // Make a POST request to create or update user without image
+      // Make a PUT request to create or update user without image
       const userResponse = await UserData.updateUser(
         data?.data?.id,
         userWithoutImage
@@ -49,19 +32,23 @@ const ModalEditUser = ({ data, test, addToTable }) => {
       console.log(userResponse);
       // Retrieve user ID from the response
       console.log(data.data.id);
-      // Create FormData to handle file upload
-      const formData = new FormData();
-      formData.append("role_id", e.target.role_id.value);
-      formData.append("full_name", e.target.full_name.value);
-      formData.append("email", e.target.email.value);
-      formData.append("password", e.target.password.value);
-      formData.append("image_url", file);
 
-      // Make a POST request to upload image for the user
-      const imageResponse = await UserData.uploadImage(
-        data?.data?.id,
-        formData
-      );
+      // Check if a new file is selected
+      if (file) {
+        // Create FormData to handle file upload
+        const formData = new FormData();
+        formData.append("role_id", e.target.role_id.value);
+        formData.append("full_name", e.target.full_name.value);
+        formData.append("email", e.target.email.value);
+        formData.append("password", e.target.password.value);
+        formData.append("image_url", file);
+
+        // Make a POST request to upload image for the user
+        const imageResponse = await UserData.uploadImage(
+          data?.data?.id,
+          formData
+        );
+      }
 
       Swal.fire({
         position: "bottom-end",
@@ -73,18 +60,27 @@ const ModalEditUser = ({ data, test, addToTable }) => {
       }).then(() => {
         addToTable(userResponse.data.data);
         modalCheckbox.current.checked = false;
+
+        document.getElementById("formId").reset();
       });
     } catch (error) {
       console.error("Error:", error.message);
+    
+      let errorMessage = "An error occurred. Please try again."; // Default error message
+    
+      if (error.response && error.response.data && error.response.data.message) {
+        errorMessage = error.response.data.message;
+      }
+    
       Swal.fire({
         position: "bottom-end",
         icon: "error",
-        title: error.message,
+        title: errorMessage,
         showConfirmButton: false,
         timer: 2000,
         customClass: "swal-custom",
       });
-    }
+    };
   };
 
   return (
@@ -125,7 +121,7 @@ const ModalEditUser = ({ data, test, addToTable }) => {
                   <select
                     className="w-full rounded border-[1.5px] text-black dark:text-white border-stroke bg-transparent py-3 px-4 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
                     name="role_id"
-                    defaultValue={data?.data?.role_id}
+                    value={data?.data?.role_id}
                   >
                     <option value="1">Admin</option>
                     <option value="2">Staff</option>
@@ -180,7 +176,6 @@ const ModalEditUser = ({ data, test, addToTable }) => {
                   <input
                     type="file"
                     name="image_url"
-                    defaultValue={data?.data?.image_url}
                     key={"user-photo"}
                     accept="image/*"
                     onChange={handleFileChange}
